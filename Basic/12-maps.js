263e31fd48663094df38482d2da315569beaf00c
WhyNot-cmd23 <jose.washereee@gmail.com>
2025-03-02 19:54:13 -0500

    Maps

---
+++ Basic/12-maps.js
@@ -0,0 +1,58 @@
+// Map - Diccionario
+
+// Declaración
+
+let myMap = new Map()
+
+console.log(myMap)
+
+//Inicializacion
+
+myMap = new Map([
+    ["nombre","Jose"],
+    ["email","luighi199@gmail.com"],
+    ["age",23],
+])
+
+console.log(myMap)
+
+// Métodos y propiedades
+
+// set
+
+myMap.set("alias","sKens")
+myMap.set("nombre","Jose") //No existen claves repetidas
+console.log(myMap)
+
+//get
+
+console.log(myMap.get("name"))
+console.log(myMap.get("surname"))//undefined
+
+//has
+
+console.log(myMap.has("surname"))
+console.log(myMap.has("age"))
+
+//delete
+
+myMap.delete("email")
+
+console.log(myMap)
+
+//keys, values y entries
+
+console.log(myMap.keys())
+console.log(myMap.values())
+console.log(myMap.entries())
+
+//size
+
+console.log(myMap.size())
+
+//clear
+
+myMap.clear()
+
+console.log(myMap)
+
